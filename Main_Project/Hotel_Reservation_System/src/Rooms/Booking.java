e9d36a9b7aaea68b75a52a1188398bb6db350db3
unknown <ali.walid007a@gmail.com>
2019-12-07 18:28:45 +0200

    Ali Walid

---
+++ Main_Project/Hotel_Reservation_System/src/Rooms/Booking.java
@@ -0,0 +1,29 @@
+package Rooms;
+
+import java.util.Date;
+
+public class Booking extends Guest {
+   
+    public void setGuestData(String GuestName,String GuestID,Date StartDate,Date EndDate,Date History)
+    {//Getdata from user
+      
+      this.GuestName=GuestName;
+      this.GuestID=GuestID;
+      this.StartDate=StartDate;
+      this.EndDate=EndDate;
+      this.History=History;
+    }
+   /* public String getGuestData()
+    {//returning the data we set
+        return GuestName + GuestID + StartDate.toString + EndDate.toString + History ;
+       
+    }*/
+   @Override 
+   public String  toString(){
+    return "main_class{" +"GuestName="+GuestName+",GuestID="+ GuestID +",StartDate="+StartDate+",EndDate="+EndDate+",History"+History+ "}";
+   }
+   
+    }
+    
+}
+
