60636bd7d5232ea5c13ad312dad6bc1199cf0578
Seif Tahawy <seiftahawy54@gmail.com>
2019-12-10 11:30:58 +0200

    New Tasks are puplished....!

--- Main_Project/Hotel_Reservation_System/src/hotel_reservation_system/Hotel_Reservation_System.java
+++ Main_Project/Hotel_Reservation_System/src/hotel_reservation_system/Hotel_Reservation_System.java
@@ -5,7 +5,7 @@ public class Hotel_Reservation_System {
 
     public static void main(String[] args) {
         database x = new database();
-        x.insert(99, "employee", "Seif", "man", "20180284", "manger");
+        // x.insert(99, "employee", "Seif", "man", "20180284", "manger");
     }
     
 }
