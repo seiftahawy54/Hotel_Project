1c08d533e09cdc9c6d276750887d1856a0a7ecd3
Seif Tahawy <seiftahawy54@gmail.com>
2019-12-13 16:39:49 +0200

    Added GUI Class!

--- Main_Project/Hotel_Reservation_System/src/GUI/Importing_Class.java
+++ Main_Project/Hotel_Reservation_System/src/GUI/Importing_Class.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Customers;
+package GUI;
 
 /**
  *
  * @author Lenovo
  */
-class RM {
+public class Importing_Class {
     
 }
